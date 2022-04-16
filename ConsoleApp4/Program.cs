dde7c1432bfa46bd0e209f251081a4fe3f4a8c83
Gabriel <Gabriel@GABRIEL_PC>
2022-04-17 00:03:52 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using Binance.Net.Clients;
+using Binance.Net.Objects;
+using CryptoExchange.Net.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace BinanceBot;
+
+public class MainClass
+{
+    public string ApiKey { get; set; } = "APIKEY";
+    public string ApiSecret { get; set; } = "APISECRET";
+
+    public static async Task Main(string[] args)
+    {
+        await new MainClass().Run();
+    }
+
+    public async Task Run()
+    {
+        BinanceClient.SetDefaultOptions(new BinanceClientOptions()
+        {
+            ApiCredentials = new ApiCredentials(ApiKey, ApiSecret),
+            LogLevel = LogLevel.Debug
+        });
+
+        using (var client = new BinanceClient())
+        {
+            //var orderData = await client.SpotApi.Trading.PlaceOrderAsync(
+            //    "BTCUSDT",
+            //    OrderSide.Buy,
+            //    SpotOrderType.Limit,
+            //    0.001m,
+            //    50000,
+            //    timeInForce: TimeInForce.GoodTillCanceled);
+
+            var bookPrc = await client.SpotApi.ExchangeData.GetBookPriceAsync("BTCUSDT");
+            if (bookPrc.Success)
+            {
+                var bestAskPrc = bookPrc.Data.BestAskPrice;
+                Console.WriteLine($"Best ASK price: {bestAskPrc}");
+            }
+        }
+    }
+}
